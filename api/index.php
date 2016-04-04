5cf238aa30c29417d69ee1946610b562bc2bb23a
Sam Beers <samb33rs@gmail.com>
2016-04-04 16:41:31 -0700

    V1.3
    Cleaned up formatting and reworked the WHERE portion to accept LIKE
    requests for search functions. There’s also a new check that will
    pre-format the JSON when viewing it on the actual API url, makes
    running tests a little easier.

--- api/index.php
+++ api/index.php
@@ -5,79 +5,98 @@
 ##  pagination: ?q=projects&p=id>:5          ##
 ##                                           ##
 ###############################################
+if(strpos($_SERVER['REQUEST_URI'],"?q=")!==false){
+  echo"<pre>";
+  $testing=true;
+}else{
+  $testing=false;
+}
 $cd_="";
 $dir=explode("/",$_SERVER['PHP_SELF']);
 unset($dir[0],$dir[1]);//default 0 & 1 to format correctly, add additional indexes for each folder deep the app root is relative to the server root
   foreach($dir as $up){
-  $cd_.="../";
+    $cd_.="../";
   }
 require_once $cd_."core/init.php";
 $j=array();//json
+if(isset($_REQUEST["s"])){
+  $sql="SELECT '{$_REQUEST["s"]}' FROM";
+}
 $sql="SELECT * FROM ";
 $w="";//where
 $o="";//order
 $l="";//limit
-  if(isset($_REQUEST["q"])){
+if(isset($_REQUEST["q"])){
   $q=$_REQUEST["q"];
-    //how many results do you need
-    if(substr($q,-1)=="s"){//plural
+  //how many results do you need
+  if(substr($q,-1)=="s"){//plural
     $o="ORDER BY `id` DESC ";
-    }else{//singular
+  }else{//singular
     $q.="s";
     $l="LIMIT 1";
-    }
-    //check if table ($q) exists in db
-    $_t="SELECT `id` FROM `{$q}` LIMIT 1";
-    $_c->query($_t);
-    if(!$_c->error){
+  }
+  //check if table ($q) exists in db
+  $_t="SELECT `id` FROM `{$q}` LIMIT 1";
+  $_c->query($_t);
+  if(!$_c->error){
     $j["success"]=true;
     $sql.="`{$q}` ";
-      if(isset($_REQUEST["p"])){//params
+    if(isset($_REQUEST["p"])){//params
       $p=explode(",",$_REQUEST["p"]);
-        foreach($p as $k=>$v){
+      foreach($p as $k=>$v){
         $_v=explode(":",$v);
         $p[$_v[0]]=$_v[1];
         unset($p[$k]);
-        }
-        foreach($p as $k=>$v){
-          if($w==""){
-          $w="WHERE";
-          }else{
-          $w.="AND";
-          }
-        $w.=" {$k}={$v} ";//dont surround params with quotes, this allows for adding opperands
-        }
       }
+      foreach($p as $k=>$v){
+        if($w==""){
+          $w="WHERE ";
+        }else{
+          $w.="AND ";
+        }
+        if(strpos($k,"LIKE")||strpos($k,"=")||strpos($k,">")||strpos($k,">=")||strpos($k,"<")||strpos($k,"<=")||strpos($k,"!=")===true){
+          $w.=trim($k)." ";
+        }else{
+          $w.=$k." = ";
+        }
+        $w.="'{$v}' ";//dont surround params with quotes, this allows for adding opperands
+      }
+    }
     $sql.=$w.$l.$o;
     $r=$_c->query($sql);
-      if(!$_c->error){
-        if($r->num_rows>0){
-          while($i=$r->fetch_assoc()){
+    if(!$_c->error){
+      if($r->num_rows>0){
+        while($i=$r->fetch_assoc()){
           $rows=array($i);
-            foreach($rows as $row){
-              foreach($row as $k=>$v){
-                if(strpos($k,"json")!==false){
+          foreach($rows as $row){
+            foreach($row as $k=>$v){
+              if(strpos($k,"json")!==false){
                 $v=json_decode($v,true);
                 $row[$k]=$v;
-                }
               }
-            $j["results"][]=$row;
             }
+            $j["results"][]=$row;
           }
-        }else{
-        $j["results"]=null;
         }
       }else{
+        $j["results"]=null;
+      }
+    }else{
       $j["success"]=false;
       $j["error"]="failed to excetute query: ".$_c->error;
       $j["results"]=null;
-      }
-    }else{
-    $j["success"]=false;
-    $j["error"]="requested table '".strtoupper($q)."' not present in database";
     }
   }else{
+    $j["success"]=false;
+    $j["error"]="requested table '".strtoupper($q)."' not present in database";
+  }
+}else{
   $j["success"]=false;
   $j["error"]="bad request, supply value for 'Q'";
-  }
-echo json_encode($j);
+}
+$j["origin"]=$_SERVER['REQUEST_URI'];
+if($testing){
+  print_r($j);
+}else{
+  echo json_encode($j);
+}
